375a16c4099eff7acd7f1ae4771a6e3e9a0cfc7f
Manoel Jorge <manoel.jmb1990@gmail.com>
2026-02-17 20:05:37 -0300

    Counter of Sheeps

---
+++ 8kyu/CountingSheep.py
@@ -0,0 +1,8 @@
+sheep = [True,  True,  True,  False,
+                  True,  True,  True,  True ,
+                  True,  False, True,  False,
+                  True,  False, False, True ,
+                  True,  True,  True,  True ,
+                  False, False, True,  True ]
+
+print(sheep.count(True))
