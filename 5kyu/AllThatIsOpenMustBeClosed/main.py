f22d00c19f9dc2dd804e1d3e7f3279a1b2979864
Manoel Jorge <manoel.jmb1990@gmail.com>
2026-02-17 18:36:17 -0300

    Sum all values of array list

--- 5kyu/AllThatIsOpenMustBeClosed/main.py
+++ 5kyu/AllThatIsOpenMustBeClosed/main.py
@@ -1,4 +1,29 @@
-source = ""
-caps = ""
+import re
 
-source, caps = input()
+source = "(Sensei [says) no!]"
+caps = "()[]"
+rules = {caps[i]: caps[i+1] for i in range(0, len(caps), 2)}
+stack = []
+
+for char in source:
+    if char in rules:
+        stack.append(char)
+
+    elif char in rules.values():
+        if not stack:
+            print("Error de fechamento")
+            break
+
+        topo = stack.pop()
+        if rules[topo] != char:
+            print("Error de fechamento")
+            break
+    else:
+        if not stack:
+            print("Tudo balanceado")
+        else:
+            print("Error de fechamento")
+
+# se eu to percorrendo uma string e um parêntese abre e
+# abre um colchetes, precisa ser fechado o colchetes e
+# os parenteses, nessa ordem.
